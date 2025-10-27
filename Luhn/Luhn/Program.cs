7a0922d84f9218668895ee5587559e4687bc10e4
ARGUMENT <vitalik.papkov@mail.ru>
2025-10-28 00:43:00 +0300

    Add Luhn algorithm implementation

--- Luhn/Luhn/Program.cs
+++ Luhn/Luhn/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+
+namespace Luhn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string n = "8383 7332 3570 8514";
+            Program start= new Program();
+            Console.WriteLine(start.validate(n));
+        }
+
+        public bool validate(string n)
+        {
+            return false;
+        }
+    }
+}
